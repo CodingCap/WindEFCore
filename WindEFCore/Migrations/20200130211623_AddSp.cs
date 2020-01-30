da848556cc0490d84439909066b1650c8fe6f19f
Albu Valentin <albuvalentin23@yahoo.com>
2020-01-30 23:18:36 +0200

    addsps

---
+++ WindEFCore/Migrations/20200130211623_AddSp.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WindEFCore.Migrations
+{
+    public partial class AddSp : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"IF OBJECT_ID('usp_clientCount','P') IS NOT NULL
+                                    DROP PROCEDURE [dbo].usp_clientCount
+                                    GO
+                                    CREATE PROCEDURE [dbo].usp_clientCount
+                                    AS
+                                    BEGIN
+	                                    SET NOCOUNT ON;
+	                                    
+	                                    SELECT 
+		                                    c.CompanyName, 
+		                                    COUNT(o.OrderId) AS Cnt
+	                                    FROM Companies AS c
+		                                    JOIN Orders AS o ON o.ClientId = c.CompanyId
+	                                    GROUP BY c.CompanyName
+	                                    
+                                    END
+                                    GO");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"IF OBJECT_ID('usp_clientCount','P') IS NOT NULL
+                                    DROP PROCEDURE [dbo].usp_clientCount
+                                    GO");
+        }
+    }
+}
